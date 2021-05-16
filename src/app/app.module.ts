e4c3bfca83b7b7c2ba37560f1f2988666c363c5e
madhukar6143 <madhukar.eppalapelly@gmail.com>
2021-05-16 14:39:04 +0530

    homepage and products page creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
